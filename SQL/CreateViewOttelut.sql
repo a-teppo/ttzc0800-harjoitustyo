7d0ab4cae3ecb6fcd4ba05e70afa5b93fd5fcfff
Pauliina Heikkilä <M3203@student.jamk.fi>
2019-05-21 20:17:08 +0300

    Lisätty sql CreateViewOttelut

---
+++ SQL/CreateViewOttelut.sql
@@ -0,0 +1,8 @@
+CREATE VIEW Ottelut AS
+SELECT OtteluId, Aika, Paikka, Kotijoukkue, kj.Nimi AS Koti, Vierasjoukkue, vj.Nimi AS Vieras, IFNULL(o1.Maalit,0) As Kotimaalit, IFNULL(o2.Maalit,0) As Vierasmaalit
+FROM Ottelu
+JOIN Joukkue kj ON kj.JoukkueId = Ottelu.Kotijoukkue
+JOIN Joukkue vj ON vj.JoukkueId = Ottelu.Vierasjoukkue
+LEFT JOIN Ottelumaalit o1 ON o1.Ottelu = Ottelu.OtteluID AND o1.Joukkue = Ottelu.Kotijoukkue
+LEFT JOIN Ottelumaalit o2 ON o2.Ottelu = Ottelu.OtteluID AND o2.Joukkue = Ottelu.Vierasjoukkue
+ORDER BY Aika, Paikka
