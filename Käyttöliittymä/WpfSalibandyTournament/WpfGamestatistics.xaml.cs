2bd5a129bf31ad267669e613ad55b441914297ec
Pauliina Heikkilä <M3203@student.jamk.fi>
2019-05-27 21:59:11 +0300

    Päivitetty ottelupöytäkirjaa

--- Käyttöliittymä/WpfSalibandyTournament/WpfGamestatistics.xaml.cs
+++ Käyttöliittymä/WpfSalibandyTournament/WpfGamestatistics.xaml.cs
@@ -22,48 +22,38 @@ namespace WpfSalibandyTournament
     /// </summary>
     public partial class WpfGamestatistics : Window
     {
+        //fields
         private List<Goal> homegoals = new List<Goal>();
         private List<Goal> awaygoals = new List<Goal>();
-        private int homegoalnumber = 0;
         private List<Penalty> homepenalties = new List<Penalty>();
-        private int homepenaltynumber = 0;
+        private List<Penalty> awaypenalties = new List<Penalty>();
         DispatcherTimer dt = new DispatcherTimer();
         Stopwatch sw = new Stopwatch();
         private string currentTime = string.Empty;
         private string periodTime = string.Empty;
+        //properties (used in comboboxes)
         public List<string> Erikoistilanteet { get; set; }
-        
+        public List<Player> HomePlayers { get; set; }
+        public List<Player> AwayPlayers { get; set; }
+        //constructors
         public WpfGamestatistics(Game game)
         {
             InitializeComponent();
-            FillCombo();
+            FillCombo(game.KotiId, game.VierasId);
             txtGameID.Text = game.OtteluId.ToString();
             txtHometeam.Text = game.KotiNimi;
-            homegoals = GetGoalsFromDB(game.OtteluId, game.KotiId);
-            //awaygoals = GetGoalsFromDB(game.OtteluId, game.VierasId);
+            CalculateTotalTime(null, null);
+            homegoals = DBSalibandytournament.GetGoalsFromDB(game.OtteluId, game.KotiId);
+            homepenalties = DBSalibandytournament.GetPenaltiesFromDB(game.OtteluId, game.KotiId);
+            awaygoals = DBSalibandytournament.GetGoalsFromDB(game.OtteluId, game.VierasId);
+            awaypenalties = DBSalibandytournament.GetPenaltiesFromDB(game.OtteluId, game.VierasId);
             dgHomeGoals.ItemsSource = homegoals;
-            //dgAwayGoals.ItemsSource = awaygoals;
+            dgHomePenalties.ItemsSource = homepenalties;
+            dgAwayGoals.ItemsSource = awaygoals;
+            dgAwayPenalties.ItemsSource = awaypenalties;
         }
-        public class Goal
-        {
-            public int MaaliID { get; set; }
-            public int Maalinro { get; set; }
-            public string Aika { get; set; }
-            public string Erikoistilanne { get; set; }
-            public string Maalintekija { get; set; }
-            public string Syottaja { get; set; }
-            public int Ottelu { get; set; }
-            public int Joukkue { get; set; }
-        }
-        public class Penalty
-        {
-            public int Rangaistusnro { get; set; }
-            public string Aika { get; set; }
-            public int Kesto { get; set; }
-            public string Syy { get; set; }
-            public int Henkilo { get; set; }
-            public int Ottelu { get; set; }
-        }
+        //methods
+        //Timer
         private void StartTimer(object sender, RoutedEventArgs e)
         {
             dt.Interval = TimeSpan.FromSeconds(1);
@@ -78,7 +68,7 @@ namespace WpfSalibandyTournament
                 TimeSpan ts = sw.Elapsed;
                 currentTime = String.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
                 lblTimer.Content = currentTime;
-                foreach(var item in ListTime())
+                foreach (var item in ListTime())
                 {
                     if (item == currentTime)
                         sw.Stop();
@@ -92,19 +82,26 @@ namespace WpfSalibandyTournament
                 sw.Stop();
             }
         }
+        private void CalculateTotalTime(object sender, RoutedEventArgs e)
+        {
+            int periods = int.Parse(txtNumberOfPeriods.Text);
+            int periodlenght = int.Parse(txtPeriodLenght.Text);
+            lblTotalTime.Content = periods*periodlenght+":00";
+        }
+        //List times of all period ends
         private List<string> ListTime()
         {
             string str = "";
             int periods = int.Parse(txtNumberOfPeriods.Text);
             double periodlenght = double.Parse(txtPeriodLenght.Text);
             List<string> timelist = new List<string>();
-            for(int i = 1; i <= periods; i++)
-            { 
+            for (int i = 1; i <= periods; i++)
+            {
                 TimeSpan t = TimeSpan.FromMinutes(i * periodlenght);
                 str = String.Format("{0:00}:{1:00}", t.Minutes, t.Seconds);
                 timelist.Add(str);
             }
-            lblHelper.Content = str;
+            lblTotalTime.Content = str;
             return timelist;
         }
         private void AddGoal(object sender, RoutedEventArgs e)
@@ -112,72 +109,80 @@ namespace WpfSalibandyTournament
             Button b = e.Source as Button;
             if (b.Name == "AddHomeGoal")
             {
-                homegoalnumber += 1;
+                int index = homegoals.Count;
                 Goal g = new Goal()
                 {
-                    Maalinro = homegoalnumber,
+                    Maalinro = ++index,
                     Aika = currentTime
                 };
                 homegoals.Add(g);
                 dgHomeGoals.ItemsSource = null;
                 dgHomeGoals.ItemsSource = homegoals;
             }
-            /*else if (b.Name == "AddAwayGoal")
+            else if (b.Name == "AddAwayGoal")
             {
-                awaygoalnumber += 1;
+                int index = awaygoals.Count;
                 Goal g = new Goal()
                 {
-                    Maalinro = awaygoalnumber,
+                    Maalinro = ++index,
                     Aika = currentTime
                 };
                 awaygoals.Add(g);
                 dgAwayGoals.ItemsSource = null;
                 dgAwayGoals.ItemsSource = awaygoals;
-            }*/
+            }
         }
         private void AddPenalty(object sender, RoutedEventArgs e)
         {
             Button b = e.Source as Button;
             if (b.Name == "AddHomePenalty")
             {
-                homepenaltynumber += 1;
+                int index = homepenalties.Count;
                 Penalty p = new Penalty()
                 {
-                    Rangaistusnro = homepenaltynumber,
+                    Rangaistusnro = ++index,
                     Aika = currentTime
                 };
                 homepenalties.Add(p);
                 dgHomePenalties.ItemsSource = null;
                 dgHomePenalties.ItemsSource = homepenalties;
             }
+            else if (b.Name == "AddHomePenalty")
+            {
+                int index = awaypenalties.Count;
+                Penalty p = new Penalty()
+                {
+                    Rangaistusnro = ++index,
+                    Aika = currentTime
+                };
+                awaypenalties.Add(p);
+                dgAwayPenalties.ItemsSource = null;
+                dgAwayPenalties.ItemsSource = awaypenalties;
+            }
         }
-        private void FillCombo()
+        private void FillCombo(int homeID, int awayID)
         {
             Erikoistilanteet = new List<string>() { "YV", "AV", "RL" };
-            cmbErikoistilanne.ItemsSource = Erikoistilanteet;
+            cmbErikoistilanneKoti.ItemsSource = Erikoistilanteet;
+            cmbErikoistilanneVieras.ItemsSource = Erikoistilanteet;
+            HomePlayers = DBSalibandytournament.GetTeamPlayersFromDB(homeID);
+            AwayPlayers = DBSalibandytournament.GetTeamPlayersFromDB(awayID);
+            cmbTekijaKoti.ItemsSource = HomePlayers;
+            cmbSyottajaKoti.ItemsSource = HomePlayers;
+            cmbKarsijaKoti.ItemsSource = HomePlayers;
+            cmbKarsijaVieras.ItemsSource = AwayPlayers;
+            cmbTekijaVieras.ItemsSource = AwayPlayers;
+            cmbSyottajaVieras.ItemsSource = AwayPlayers;
         }
-        public static List<Goal> GetGoalsFromDB(int gameID, int teamID)
+        //get current indexnumber for goals and penalties when adding new rows
+        private int GetIndex<T>(List<T> data)
         {
-            List<Goal> goals = new List<Goal>();
-            string str = $"Maali WHERE Ottelu = {gameID} AND Joukkue = {teamID} ORDER BY Aika";
-            DataTable dt = DBSalibandytournament.GetViewDB(str);
-            int maalinro = 0;
-            foreach (DataRow item in dt.Rows)
+            int i = 1;
+            foreach (var foo in data)
             {
-                Goal g = new Goal()
-                {
-                    Maalinro = ++maalinro,
-                    MaaliID = int.Parse(item[0].ToString()),
-                    Aika = item[1].ToString(),
-                    Erikoistilanne = item[2].ToString(),
-                    Maalintekija = item[3].ToString(),
-                    Syottaja = item[4].ToString(),
-                    Joukkue = int.Parse(item[5].ToString()),
-                    Ottelu = int.Parse(item[6].ToString())
-                };
-                goals.Add(g);
+                i++;
             }
-            return goals;
+            return i;
         }
     }
 }
