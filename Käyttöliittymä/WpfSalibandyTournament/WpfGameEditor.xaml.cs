3734bfcadbdcfcd5b6c811ab64e2b3fc77c09435
Pauliina Heikkilä <M3203@student.jamk.fi>
2019-05-28 23:20:11 +0300

    Lisätty lomake uusien otteluiden lisäykselle ja muokkaukselle

---
+++ Käyttöliittymä/WpfSalibandyTournament/WpfGameEditor.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfSalibandyTournament
+{
+    /// <summary>
+    /// Interaction logic for WpfGameEditor.xaml
+    /// </summary>
+    public partial class WpfGameEditor : Window
+    {
+        //fields
+        private WpfGames gamesWindow;
+        private Game game;
+        //properties
+        public List<Team> Teams { get; set; }
+        //constructors
+        public WpfGameEditor(WpfGames gamesWindow)
+        {
+            InitializeComponent();
+            FillCombo();
+            this.gamesWindow = gamesWindow;
+        }
+        public WpfGameEditor(WpfGames gamesWindow, Game game)
+        {
+            InitializeComponent();
+            this.game = game;
+            txtID.Text = game.OtteluId.ToString();
+            txtLocation.Text = game.Paikka;
+            txtTime.Text = game.Aika;
+            cmbHomeTeam.SelectedValue = game.KotiId;
+            cmbAwayTeam.SelectedValue = game.VierasId;
+            FillCombo();
+            this.gamesWindow = gamesWindow;
+        }
+        //methods
+        private void FillCombo()
+        {
+            Teams = DBSalibandytournament.GetTeamsFromDB();
+            cmbHomeTeam.ItemsSource = Teams;
+            cmbAwayTeam.ItemsSource = Teams;
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string Time = $"'{txtTime.Text}'";
+            string Location = $"'{txtLocation.Text}'";
+            bool parseokHome = Int32.TryParse(cmbHomeTeam.SelectedValue.ToString(), out int HomeTeamID);
+            bool parseokAway = Int32.TryParse(cmbAwayTeam.SelectedValue.ToString(), out int AwayTeamID);
+
+            if (string.IsNullOrWhiteSpace(txtID.Text))
+            {
+                string inserttablestring = "Ottelu (Aika, Paikka, Kotijoukkue, Vierasjoukkue, Paatetty)";
+                string insertvaluestring = $"({Time}, {Location}, {HomeTeamID}, {AwayTeamID}, FALSE)";
+                DBSalibandytournament.InsertIntoDB(inserttablestring, insertvaluestring);
+            }
+            else
+            {
+                int GameID = int.Parse(txtID.Text);
+                string updatetablestring = "Ottelu";
+                string updatevaluestring = $"Aika = {Time}, Paikka = {Location}, Kotijoukkue = {HomeTeamID}, Vierasjoukkue = {AwayTeamID}";
+                string updatewherestring = $"OtteluID = {GameID}";
+                DBSalibandytournament.UpdateDB(updatetablestring, updatevaluestring, updatewherestring);
+            }
+            Close();
+            gamesWindow.RefreshGames();
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
